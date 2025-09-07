54c51029caadf039d28b043c39d4c10515ffb64d
Leo Wehrfritz <leo@wehrfritz.me>
2025-09-07 10:34:37 +0200

    Command buttons now send commands

--- lib/views/subviews/controls/command_button_if_exists.dart
+++ lib/views/subviews/controls/command_button_if_exists.dart
@@ -1,9 +1,16 @@
+import 'dart:developer' as developer;
+
 import 'package:flutter/material.dart';
 import 'package:equilibrium_flutter/models/classes/command.dart';
 import 'package:equilibrium_flutter/models/enums/remote_button.dart';
 import 'package:collection/collection.dart';
+import 'package:get_it/get_it.dart';
+import 'package:equilibrium_flutter/helpers/hub_connection_handler.dart';
 
 class CommandButtonIfExists extends StatelessWidget {
+  final HubConnectionHandler connectionHandler =
+  GetIt.instance<HubConnectionHandler>();
+
   final Command? command;
   final bool useName;
   final Widget? icon;
@@ -23,7 +30,14 @@ class CommandButtonIfExists extends StatelessWidget {
     return (command != null)
         ? IconButton(
         constraints: BoxConstraints(maxHeight: 50, maxWidth: 50),
-        onPressed: () {},
+        onPressed: () {
+          final id = command?.id;
+          if (id != null) {
+            connectionHandler.api?.sendCommand(id);
+          } else {
+            developer.log("Couldn't get command id");
+          }
+        },
         icon:
         icon ??
             (useName
